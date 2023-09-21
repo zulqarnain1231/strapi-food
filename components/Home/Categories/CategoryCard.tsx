3d63f9b398af2e672c667ad29029bb337e3871a1
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-20 22:18:10 -0700

    fixed minor issue in the category slider

--- components/Home/Categories/CategoryCard.tsx
+++ components/Home/Categories/CategoryCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const CategoryCard: React.FC<Props> = ({ title }: Props) => {
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-4 px-[62px] py-9 rounded-[30px] bg-white-main shadow-md hover:bg-gradient-to-t from-brand-secondary to-brand-main hover:duration-100 cursor-pointer text-black-main hover:text-white-main">
+    <div className="w-full flex flex-col items-center justify-center gap-4 px-[62px] py-9 rounded-[30px] bg-white-main shadow-md hover:bg-gradient-to-t from-brand-secondary to-brand-main hover:duration-100 cursor-pointer text-black-main hover:text-white-main sm:mb-0 mb-4">
       <div className="relative w-[106px] h-[106px]">
         <Image
           className="w-full h-full object-contain pointer-events-none"

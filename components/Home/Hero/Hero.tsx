4aabed0f3981c5ee770ef41fd118b357d768467d
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-20 20:14:33 -0700

    made hero seciton responsive

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
       <div className="w-full h-full grid xl:grid-cols-[1.5fr,1fr] gap-6">
         {/* left section */}
         <aside className="w-full h-full flex flex-col items-start justify-center gap-6">
-          <h1 className="text-black-main text-[80px] leading-[115px] font-semibold">
+          <h1 className="text-black-main xl:text-left text-center sm:text-[80px] text-6xl sm:leading-[115px] sm:mt-0 mt-6 font-semibold">
             The Fastest In Delivering{" "}
             <span className="text-white-main">Food</span>
           </h1>
-          <p className="text-black-main text-[20px] leading-10 font-normal">
+          <p className="text-black-main sm:text-[20px] text-lg xl:text-left text-center leading-10 font-normal">
             Lorem ipsum dolor sit amet, consectetur adipiscing Fusce
             condimentum, magna vel interdum gravida, nibh erat tempus lacus, non
             dictum.
@@ -22,9 +22,9 @@ const Hero = () => {
         </aside>
         {/* right section */}
         <aside className="w-full h-full flex flex-col items-center justify-center">
-          <div className="w-[500px] h-[500px] relative">
+          <div className="sm:w-[500px] w-full sm:h-[500px] h-[400px] relative">
             <Image
-              className="w-full h-full object-fill pointer-events-none"
+              className="w-full h-full sm:object-fill object-contain pointer-events-none"
               src={"/Assets/Hero/Plate.png"}
               alt=""
               fill
--- components/Home/Hero/SearchSnippet.tsx
+++ components/Home/Hero/SearchSnippet.tsx
@@ -7,40 +7,42 @@ import { BsArrowUpRight } from "react-icons/bs";
 const SearchSnippet = () => {
   return (
     <div className="w-full flex flex-col items-center justify-start gap-6">
-      <div className="w-full flex items-center justify-between md:h-[100px] bg-white-main rounded-[16px]">
-        <div className="h-full w-[calc(100%-221px)] flex items-center justify-start gap-4 px-6">
-          <div className="w-[60px] h-[60px] flex items-center justify-center bg-black-off rounded-[12px]">
+      <div className="w-full flex sm:flex-row flex-col items-center sm:justify-between justify-start sm:h-[100px] bg-white-main rounded-[16px]">
+        <div className="h-full w-full sm:w-[calc(100%-221px)] flex items-center justify-start sm:gap-4 gap-4 p-4 sm:py-0 sm:px-6">
+          <div className="w-[60px] h-[60px] flex flex-shrink-0 items-center justify-center bg-black-off rounded-[12px]">
             <HiOutlineLocationMarker className="text-white-main text-2xl" />
           </div>
           <div className="h-full flex flex-col items-start justify-center gap-1">
             <div className="flex items-center justify-start gap-2">
-              <p className="text-base text-black-main font-medium font-mullish">
+              <p className="sm:text-base text-sm text-black-main font-medium font-mullish">
                 Destination
               </p>
               <IoChevronDown className="text-black-main text-xl" />
             </div>
-            <p className="text-base text-brand-main font-normal">
+            <p className="sm:text-base text-sm text-brand-main font-normal">
               Monaco, Italy
             </p>
           </div>
           {/* divider */}
           <span className="w-[1px] h-[45px] bg-white-off"></span>
           <div className="h-full flex items-center gap-2 justify-start">
-            <FiSearch className="text-2xl text-black-main" />
-            <p className="text-black-main text-base font-mullish font-medium ">
+            <FiSearch className="sm:text-2xl text-xl text-black-main" />
+            <p className="text-black-main sm:text-base text-sm font-mullish font-medium ">
               Sky Bar
             </p>
           </div>
         </div>
-        <div className="h-full w-[221px] flex flex-col items-end justify-center bg-black-main rounded-r-2xl cursor-pointer">
+        <div className="sm:h-full py-4 w-full sm:w-[221px] flex flex-col items-end justify-center bg-black-main sm:rounded-r-2xl sm:rounded-br-2xl sm:rounded-b-none rounded-b-2xl cursor-pointer">
           <div className="w-full flex items-center justify-center gap-2">
-            <p className="text-white-main text-[20px] font-normal">Search</p>
+            <p className="text-white-main text-lg sm:text-xl font-normal">
+              Search
+            </p>
             <BsArrowUpRight className="text-white-main text-2xl" />
           </div>
         </div>
       </div>
       {/* popular searches */}
-      <div className="w-full flex items-center justify-start gap-4">
+      <div className="w-full flex sm:flex-row flex-col items-center justify-start gap-4">
         <p className="text-white-main text-[20px] font-normal">
           Popular Search
         </p>

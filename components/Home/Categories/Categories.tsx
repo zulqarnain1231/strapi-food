ec12d57a3f99201c0cf83f19c47e57db0502bc8b
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-09-23 00:31:02 -0700

    fixed the slider issues

--- components/Home/Categories/Categories.tsx
+++ components/Home/Categories/Categories.tsx
@@ -42,14 +42,14 @@ const Categories = () => {
     prevArrow: <CustomPrevArrow />,
     speed: 500,
     autoplay: true,
-    slidesToShow: 5.5,
+    slidesToShow: 6,
     slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3.5,
+          slidesToShow: 4,
           slidesToScroll: 1,
           infinite: true,
         },
--- components/Home/Resorts/NewResort.tsx
+++ components/Home/Resorts/NewResort.tsx
@@ -40,14 +40,14 @@ const NewResort = () => {
     prevArrow: <CustomPrevArrow />,
     speed: 500,
     autoplay: true,
-    slidesToShow: 3.5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2.5,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
         },
